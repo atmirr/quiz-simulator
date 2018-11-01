d83a1f3d120ae84ce4ebc3ae992eae9f177a96eb
Amir Attari <amir.aetari@gmail.com>
2018-11-02 02:27:01 +0330

    Updates

---
+++ precache-manifest.2f2667064d306f4b19f701597ea52a76.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d62bc66053054e2ee3f",
+    "url": "/quiz-simulator/static/js/runtime~main.1d62bc66.js"
+  },
+  {
+    "revision": "00ce2cb817d040ea24aa",
+    "url": "/quiz-simulator/static/js/main.00ce2cb8.chunk.js"
+  },
+  {
+    "revision": "bc63a9dd63c477837fb2",
+    "url": "/quiz-simulator/static/js/1.bc63a9dd.chunk.js"
+  },
+  {
+    "revision": "b74ce852ba48ed034544bdf4a3e01ffd",
+    "url": "/quiz-simulator/index.html"
+  }
+];
