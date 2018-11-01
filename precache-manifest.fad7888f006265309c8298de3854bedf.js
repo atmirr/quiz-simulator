95af2d64a096c1a0028e5e43d34b566a2d55d0b4
Amir Attari <amir.aetari@gmail.com>
2018-11-02 02:14:33 +0330

    Updates

---
+++ precache-manifest.fad7888f006265309c8298de3854bedf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00ce2cb817d040ea24aa",
+    "url": "/static/js/main.00ce2cb8.chunk.js"
+  },
+  {
+    "revision": "bc63a9dd63c477837fb2",
+    "url": "/static/js/1.bc63a9dd.chunk.js"
+  },
+  {
+    "revision": "189e5ead018e31c6ef94a89f2f1d05de",
+    "url": "/index.html"
+  }
+];
